3b236a0e702781dab639d6fd918027e69c1b1f6b
Setoz <markiss226@gmail.com>
2024-12-16 16:52:46 +0300

    Add files via upload

---
+++ Office 2016 Activation.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Activation...
+
+(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&(for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&cscript //nologo ospp.vbs /unpkey:WFG99 >nul&cscript //nologo ospp.vbs /unpkey:DRTFM >nul&cscript //nologo ospp.vbs /unpkey:BTDRB >nul&cscript //nologo ospp.vbs /unpkey:CPQVG >nul&cscript //nologo ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99 >nul&set i=1
+:server
+if %i%==1 set KMS_Sev=kms7.MSGuides.com
+if %i%==2 set KMS_Sev=kms8.MSGuides.com
+if %i%==3 set KMS_Sev=kms9.MSGuides.com
+cscript //nologo ospp.vbs /sethst:%KMS_Sev% >nul
+cscript //nologo ospp.vbs /act || if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)
+
+echo.
+echo Done!!!
+pause
---
+++ Office 2019-2024 Activation.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Activation...
+
+:: Selecting the correct folder if exists
+if exist "%ProgramFiles(x86)%\Microsoft Office\Office16" (
+    cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+) else if exist "%ProgramFiles%\Microsoft Office\Office16" (
+    cd /d "%ProgramFiles%\Microsoft Office\Office16"
+) else (
+    echo Could not find folder Microsoft Office\Office16.
+    pause
+    exit /b
+)
+
+:: Installing a license
+for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do (
+    cscript ospp.vbs /inslic:"..\root\Licenses16\%%x"
+)
+
+:: Setting up KMS
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /unpkey:6F7TH >nul
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:e8.us.to
+cscript ospp.vbs /act
+
+echo.
+echo Done!!!
+
+pause
